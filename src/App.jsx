97b40a0b6eafeae33221453efd1806f4ed0787e5
Rajat jangra <rajatjangra940@gmail.com>
2026-03-22 10:59:34 +0530

    Add IGNIS VENTURES MEDIA project to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ export default function DigitalBusinessWebsite() {
     { name: "The ShriramFoundation School", image: "https://res.cloudinary.com/drrj8rl9n/image/upload/v1772793593/Screenshot_2026-03-06_160535_uk0dia.png", webLink: "https://www.theshriramfoundationschool.in" },
     { name: "Raahi Ecommers", image: "https://res.cloudinary.com/drrj8rl9n/image/upload/v1772793951/Screenshot_2026-03-06_161449_kaxxtw.png", webLink: "https://rm-com.vercel.app" },
     { name: "Transport Business Management System", image: "https://res.cloudinary.com/drrj8rl9n/image/upload/v1773640071/Screenshot_2026-03-16_111731_siiowa.png", webLink: "https://taransport-solutions-client-g1lq.vercel.app" },
+    { name: "IGNIS VENTURES MEDIA", image: "https://res.cloudinary.com/drrj8rl9n/image/upload/v1774156212/WhatsApp_Image_2026-03-22_at_10.39.25_AM_ucpfp1.jpg", webLink: "https://ignis-media.vercel.app" },
 
   ]
 
